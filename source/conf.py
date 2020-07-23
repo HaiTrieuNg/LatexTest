1e0a4629bd0e6fe58b1a3235cd90ebbe4e26ef11
Trieu Nguyen <47194907+HaiTrieuNg@users.noreply.github.com>
2020-07-23 14:21:27 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'BeeFi_Logo.png'
+#html_logo = 'BeeFi_Logo.png'
 html_favicon = 'BeeFi_Logo.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
